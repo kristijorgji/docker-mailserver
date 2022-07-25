aacb74cf21ec0973e040e87ee15b966401ecc91d
Kristi Jorgji <kristijorgji@hotmail.com>
2022-07-25 14:14:12 +0200

    move certificate generation inside ansible during entrypoint execution. For prod env use letsencrypt

--- docker-data/entrypoint.sh
+++ docker-data/entrypoint.sh
@@ -1,20 +1,11 @@
 #!/usr/bin/env bash
 
-PATH_SSL="/etc/letsencrypt/live/${MAIL_DOMAIN}"
-
 # Configure mail linux user
+# TODO move this into the ansible provisioning so user and group names come from variables
 groupadd -g 5000 vmail  \
     && useradd -s /usr/sbin/nologin -u 5000 -g 5000 vmail \
     && usermod -aG vmail postfix \
     && usermod -aG vmail dovecot
-
-# Create mails directory with correct owner
-mkdir -p /var/mail/vhosts/$DOMAIN \
-    && chown -R vmail:vmail /var/mail/vhosts \
-    && chmod -R 775 /var/mail/vhosts
-
-# create dovecot ssl if not existing
-./gen-certificate.sh "$DOMAIN" "$PATH_SSL"
 
 # apply ansible configs
 cd ansible
@@ -27,4 +18,4 @@ service postfix start
 service dovecot start;
 
 # keep container up and running as we don't start any server
-tail -f /dev/null
+tail -f /var/log/*
